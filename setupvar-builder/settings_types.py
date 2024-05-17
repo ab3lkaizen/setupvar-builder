00f485e6b02c22030bd53ce0d9cdadfb0fd52143
ab3lkaizen <abelkelemen62@gmail.com>
2024-05-17 18:04:46 +0200

    initial commit

---
+++ setupvar-builder/settings_types.py
@@ -0,0 +1,25 @@
+from typing import TypedDict
+
+
+class OneOfDict(TypedDict):
+    type: str
+    name: str
+    help_string: str
+    size: str
+    options: dict[str, str]
+    default: str | None
+
+
+class NumericDict(TypedDict):
+    type: str
+    name: str
+    size: str
+    min: int
+    max: int
+    default: int
+
+
+class CheckBoxDict(TypedDict):
+    type: str
+    name: str
+    default: str
